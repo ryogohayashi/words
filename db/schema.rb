b73e18fcb23b7cd313dd5e2ed6863b60ad6f3c27
ryogohayasi <hamoyashi0923@gmail.com>
2020-06-03 14:25:21 +0900

    sortable.j導入準備 schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_041440) do
+ActiveRecord::Schema.define(version: 2020_06_03_025754) do
 
   create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_04_03_041440) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "group_id"
+    t.integer "position"
     t.index ["user_id"], name: "index_words_on_user_id"
     t.index ["word"], name: "index_words_on_word", length: 32
   end
