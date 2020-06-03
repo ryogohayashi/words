41c9fec662fbb055c58ab6928f86a82ab2a44280
ryogohayasi <hamoyashi0923@gmail.com>
2020-06-04 03:04:33 +0900

    routes.rb修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   root to: 'homes#index'
   devise_for :users
   resources :groups, only: [:index, :new, :create, :edit, :update, :destroy] do
-    resources :words, only: [:index, :new, :create, :edit, :destroy] do
+    resources :words, only: [:index, :new, :create, :edit, :update, :destroy] do
       member do
         get 'search'
       end
+    end
   end
-end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
