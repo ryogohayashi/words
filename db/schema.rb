c84028b2dc317b2106470da63369c20930afd715
ryogohayasi <hamoyashi0923@gmail.com>
2020-06-05 08:18:26 +0900

    scheme　更新（ドラッグアンドドロップ）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_025754) do
+ActiveRecord::Schema.define(version: 2020_06_03_183501) do
 
   create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_06_03_025754) do
     t.datetime "updated_at", null: false
     t.integer "group_id"
     t.integer "position"
+    t.integer "row_order"
     t.index ["user_id"], name: "index_words_on_user_id"
     t.index ["word"], name: "index_words_on_word", length: 32
   end
