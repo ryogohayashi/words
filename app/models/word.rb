157be2687b1385a8c5fa0cb030ed9d169d8177b7
ryogohayasi <hamoyashi0923@gmail.com>
2020-06-05 08:37:38 +0900

    word.rb　RankModel追加

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,9 @@
 class Word < ApplicationRecord
   belongs_to :user
   belongs_to :group
+  
+  include RankedModel 
+  ranks :row_order , with_same: :group_id 
 
   def self.search(search)
     return Word.all unless search
